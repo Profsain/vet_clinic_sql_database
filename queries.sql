c1bd13222618d03175a77c41628ef3cbf4deaa40
Favourezeugwa <favourezeugwa@gmail.com>
2022-08-22 17:59:02 +0100

    Add queries for explain analyze

--- queries.sql
+++ queries.sql
@@ -185,4 +185,12 @@ LIMIT 1;
 
 
 
+/* PROJECT WK 2 DAY 1 */
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animals_fk_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vets_fk_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners WHERE email = 'owner_18327@mail.com';
+
 
